1055a7a9240184db63252cfe430a6bc05a939ce3
Chelsey Gowac <97612806+chelsey-g@users.noreply.github.com>
2022-11-15 11:31:57 -0500

    replaced class with className (#10)
    Fixes #5

--- pages/index.js
+++ pages/index.js
@@ -724,21 +724,21 @@ export default function Home() {
 
                       {showDeleteModal && (
                         <div
-                          class="relative z-10"
+                          className="relative z-10"
                           aria-labelledby="modal-title"
                           role="dialog"
                           aria-modal="true"
                         >
-                          <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
+                          <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
 
-                          <div class="fixed inset-0 z-10 overflow-y-auto">
-                            <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-                              <div class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
-                                <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-                                  <div class="sm:flex sm:items-start">
-                                    <div class="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                          <div className="fixed inset-0 z-10 overflow-y-auto">
+                            <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+                              <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                                <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                                  <div className="sm:flex sm:items-start">
+                                    <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
                                       <svg
-                                        class="h-6 w-6 text-red-600"
+                                        className="h-6 w-6 text-red-600"
                                         xmlns="http://www.w3.org/2000/svg"
                                         fill="none"
                                         viewBox="0 0 24 24"
@@ -753,15 +753,15 @@ export default function Home() {
                                         />
                                       </svg>
                                     </div>
-                                    <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                                    <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
                                       <h3
-                                        class="text-lg font-medium leading-6 text-gray-900"
+                                        className="text-lg font-medium leading-6 text-gray-900"
                                         id="modal-title"
                                       >
                                         Delete contact
                                       </h3>
-                                      <div class="mt-2">
-                                        <p class="text-sm text-gray-500">
+                                      <div className="mt-2">
+                                        <p className="text-sm text-gray-500">
                                           Are you sure you want to delete this
                                           contact? All of your data will be
                                           permanently deleted. This cannot be
@@ -771,20 +771,20 @@ export default function Home() {
                                     </div>
                                   </div>
                                 </div>
-                                <div class="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                                <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
                                   <button
                                     type="button"
                                     onClick={() =>
                                       handleDeleteContact(currentContact.id)
                                     }
-                                    class="inline-flex w-full justify-center rounded-md border border-transparent bg-red-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm"
+                                    className="inline-flex w-full justify-center rounded-md border border-transparent bg-red-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm"
                                   >
                                     Delete Contact
                                   </button>
                                   <button
                                     onClick={() => setShowDeleteModal(false)}
                                     type="button"
-                                    class="mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+                                    className="mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
                                   >
                                     Cancel
                                   </button>
@@ -807,21 +807,21 @@ export default function Home() {
                 <button onClick={handleSearchChange}>Search Contact</button>
                 {!searchHidden && (
                   <div
-                    class="relative z-10"
+                    className="relative z-10"
                     aria-labelledby="modal-title"
                     role="dialog"
                     aria-modal="true"
                   >
-                    <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
+                    <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
 
-                    <div class="fixed inset-0 z-10 overflow-y-auto">
-                      <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-                        <div class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
-                          <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-                            <div class="sm:flex sm:items-start">
-                              <div class="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                    <div className="fixed inset-0 z-10 overflow-y-auto">
+                      <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+                        <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                          <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                            <div className="sm:flex sm:items-start">
+                              <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
                                 <svg
-                                  class="h-6 w-6 text-red-600"
+                                  className="h-6 w-6 text-red-600"
                                   xmlns="http://www.w3.org/2000/svg"
                                   fill="none"
                                   viewBox="0 0 24 24"
@@ -836,15 +836,15 @@ export default function Home() {
                                   />
                                 </svg>
                               </div>
-                              <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                              <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
                                 <h3
-                                  class="text-lg font-medium leading-6 text-gray-900"
+                                  className="text-lg font-medium leading-6 text-gray-900"
                                   id="modal-title"
                                 >
                                   Search Contact
                                 </h3>
-                                <div class="mt-2">
-                                  <p class="text-sm text-gray-500">
+                                <div className="mt-2">
+                                  <p className="text-sm text-gray-500">
                                     <input
                                       type="search"
                                       placeholder="Search contact"
@@ -855,17 +855,17 @@ export default function Home() {
                               </div>
                             </div>
                           </div>
-                          <div class="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                          <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
                             <button
                               type="button"
-                              class="inline-flex w-full justify-center rounded-md border border-transparent bg-red-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm"
+                              className="inline-flex w-full justify-center rounded-md border border-transparent bg-red-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm"
                             >
                               Search Contact
                             </button>
                             <button
                               onClick={() => setSearchHidden(true)}
                               type="button"
-                              class="mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+                              className="mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
                             >
                               Cancel
                             </button>

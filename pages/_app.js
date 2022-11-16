eabb89f96380ea3db087f915f047bd9242209c17
Jeff Loiselle <jeff@loiselles.com>
2022-11-16 14:01:31 -0600

    added sentry for capturing errors

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,20 @@
 import "../styles/globals.css"
 
+import React from "react";
+import ReactDOM from "react-dom";
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
+
+Sentry.init({
+  dsn: "https://119ec84d123e4b32a5b29014dc58ef2d@o4504170566320128.ingest.sentry.io/4504170576084992",
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
+
 function MyApp({ Component, pageProps }) {
   return (
     <>

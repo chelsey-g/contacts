e4c34a5436db585c9e71ba5017d83b1c5f4be81f
chelsey-g <97612806+chelsey-g@users.noreply.github.com>
2022-11-17 16:36:32 -0500

    fixes updating and saving

--- pages/index.js
+++ pages/index.js
@@ -736,14 +736,27 @@ export default function Home() {
                         Cancel
                       </button>
 
-                      <button
-                        disabled={Boolean(phoneEntryError)}
-                        type="submit"
-                        className="ml-3 inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                        onClick={handleSubmit(handleUpdateContact)}
-                      >
-                        Save
-                      </button>
+                      {currentContact?.id && (
+                        <button
+                          disabled={Boolean(phoneEntryError)}
+                          type="submit"
+                          className="ml-3 inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                          onClick={handleSubmit(handleUpdateContact)}
+                        >
+                          Update
+                        </button>
+                      )}
+
+                      {!currentContact?.id && (
+                        <button
+                          disabled={Boolean(phoneEntryError)}
+                          type="submit"
+                          className="ml-3 inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                        >
+                          Save
+                        </button>
+                      )}
+
                       {currentContact?.id && (
                         <button
                           type="button"

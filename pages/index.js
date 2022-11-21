8cacbe879600fdffe44065d7e1e4d8afd58368d9
chelsey-g <97612806+chelsey-g@users.noreply.github.com>
2022-11-21 16:57:10 -0500

    added ability to search

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,11 @@ import {
   deleteContact,
   updateContact,
 } from "../src/graphql/mutations";
-import { getContact, contactsByName } from "../src/graphql/queries";
+import {
+  getContact,
+  contactsByName,
+  searchContacts,
+} from "../src/graphql/queries";
 import * as subscriptions from "../src/graphql/subscriptions";
 import Link from "next/link";
 import { Amplify, Auth } from "aws-amplify";
@@ -89,24 +93,24 @@ export default function Home() {
   const [phoneEntryError, setPhoneEntryError] = useState(null);
   const [nameEntryError, setNameEntryError] = useState(null);
   const [showDeleteModal, setShowDeleteModal] = useState(false);
-  const [searchInput, setSearchInput] = useState("");
   const [searchHidden, setSearchHidden] = useState(true);
+  const [search, setSearch] = useState("");
 
-  useEffect(() => {
-    fetchContacts();
-  }, []);
+  // useEffect(() => {
+  //   fetchContacts();
+  // }, []);
 
-  function fetchContacts() {
-    API.graphql({
-      query: contactsByName,
-      variables: {
-        type: "Contact",
-        sortDirection: "DESC",
-      },
-    }).then((data) => {
-      setContacts(data.data.contactsByName.items);
-    });
-  }
+  // function fetchContacts() {
+  //   API.graphql({
+  //     query: contactsByName,
+  //     variables: {
+  //       type: "Contact",
+  //       sortDirection: "DESC",
+  //     },
+  //   }).then((data) => {
+  //     setContacts(data.data.contactsByName.items);
+  //   });
+  // }
 
   const handleDeleteContact = async (id) => {
     try {
@@ -119,7 +123,11 @@ export default function Home() {
           },
         },
       }).then((data) => {
-        fetchContacts();
+        setContacts(
+          contacts.filter(
+            (contact) => data.data.deleteContact.id !== contact.id
+          )
+        );
         setShowDeleteModal(!showDeleteModal);
         setShowForm(false);
       });
@@ -142,18 +150,16 @@ export default function Home() {
           },
         },
       }).then((data) => {
-        fetchContacts();
+        let newContacts = contacts.filter(
+          (contact) => data.data.updateContact.id !== contact.id
+        );
+        newContacts.push(data.data.updateContact);
+        setContacts(newContacts);
         setShowForm(false);
       });
     } catch (e) {
       console.log(e);
     }
-  }
-
-  function handleSearchChange(e) {
-    e.preventDefault();
-    setSearchInput(e.target.value);
-    setSearchHidden(!searchHidden);
   }
 
   function handleContactClick(id) {
@@ -179,12 +185,30 @@ export default function Home() {
         },
       },
     }).then((data) => {
-      fetchContacts();
+      setContacts([...contacts, data.data.createContact]);
       setCurrentContact(data.data.createContact);
       reset();
       setShowForm(false);
     });
   }
+
+  useEffect(() => {
+    async function doSearch() {
+      try {
+        let res = await API.graphql({
+          query: searchContacts,
+          variables: {
+            filter: { name: { wildcard: `*${search}*` } },
+            sort: { direction: "asc", field: "name" },
+          },
+        });
+        setContacts(res.data.searchContacts.items);
+      } catch (e) {
+        alert(e.errors[0].message);
+      }
+    }
+    doSearch();
+  }, [search]);
 
   function handleCreateContact() {
     setCurrentContact(null);
@@ -849,87 +873,27 @@ export default function Home() {
             <aside className="hidden lg:order-first lg:block lg:flex-shrink-0">
               <div className="relative flex h-full w-96 flex-col overflow-y-auto border-r border-gray-200 bg-gray-100">
                 <button
-                  className="inline-flex w-full justify-center rounded-md border border-transparent bg-indigo-500 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm"
+                  className="inline-flex w-full justify-center rounded-md border border-transparent bg-indigo-500 px-4 py-2 my-3 text-base font-medium text-white shadow-sm hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm"
                   onClick={handleCreateContact}
                 >
                   Create Contact
                 </button>
-                <button
-                  className="inline-flex w-full justify-center rounded-md border border-transparent bg-indigo-500 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm"
-                  onClick={handleSearchChange}
-                >
-                  Search Contact
-                </button>
-                {!searchHidden && (
-                  <div
-                    className="relative z-10"
-                    aria-labelledby="modal-title"
-                    role="dialog"
-                    aria-modal="true"
-                  >
-                    <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
-
-                    <div className="fixed inset-0 z-10 overflow-y-auto">
-                      <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-                        <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
-                          <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-                            <div className="sm:flex sm:items-start">
-                              <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
-                                <svg
-                                  className="h-6 w-6 text-red-600"
-                                  xmlns="http://www.w3.org/2000/svg"
-                                  fill="none"
-                                  viewBox="0 0 24 24"
-                                  stroke-width="1.5"
-                                  stroke="currentColor"
-                                  aria-hidden="true"
-                                >
-                                  <path
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                    d="M12 10.5v3.75m-9.303 3.376C1.83 19.126 2.914 21 4.645 21h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 4.88c-.866-1.501-3.032-1.501-3.898 0L2.697 17.626zM12 17.25h.007v.008H12v-.008z"
-                                  />
-                                </svg>
-                              </div>
-                              <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-                                <h3
-                                  className="text-lg font-medium leading-6 text-gray-900"
-                                  id="modal-title"
-                                >
-                                  Search Contact
-                                </h3>
-                                <div className="mt-2">
-                                  <p className="text-sm text-gray-500">
-                                    <input
-                                      type="search"
-                                      placeholder="Search contact"
-                                      onChange={handleSearchChange}
-                                    />
-                                  </p>
-                                </div>
-                              </div>
-                            </div>
-                          </div>
-                          <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
-                            <button
-                              type="button"
-                              className="inline-flex w-full justify-center rounded-md border border-transparent bg-red-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm"
-                            >
-                              Search Contact
-                            </button>
-                            <button
-                              onClick={() => setSearchHidden(true)}
-                              type="button"
-                              className="mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
-                            >
-                              Cancel
-                            </button>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
+                <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                  <div className="mt-2">
+                    <center>
+                      <p className="text-sm text-white">
+                        <input
+                          type="search"
+                          placeholder="Search contact"
+                          onChange={(e) => setSearch(e.target.value)}
+                          className="inline-flex w-full justify-center rounded-md border border-transparent bg-indigo-500 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm"
+                        />
+                      </p>
+                    </center>
                   </div>
-                )}
+
+                  <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6"></div>
+                </div>
                 {contacts.map((contact) => (
                   <div
                     key={contact.name}
